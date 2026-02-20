9b79396722c81628ab575a1644881a57f5a9315f
BananoDurisimo <vicenterios.vargas007@gmail.com>
2026-02-20 17:21:07 -0500

    m

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { HashRouter } from "react-router-dom"
 
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>
 )
