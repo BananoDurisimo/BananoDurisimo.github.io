f70d7ad505847430853c007471e6b4da8038d439
BananoDurisimo <vicenterios.vargas007@gmail.com>
2026-02-20 17:24:10 -0500

    m

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ import { Header } from './features/layout/components/Header'
 import { Footer } from './features/layout/components/Footer'
 import { Content } from './features/layout/components/Content'
 import { Props } from './features/layout/components/Props'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Content />} />
         <Route path="/props" element={<Props />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   )
 }
 
